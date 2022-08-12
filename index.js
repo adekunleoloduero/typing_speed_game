f1b9fbfdb58f88f81adb3df2365afb871f537094
Sheriff Adekunle Tajudeen <dnexteinstein2@gmail.com>
2022-08-12 11:55:22 +0100

    Matching original quote to payer entry:
    Started working on the highlightNextSection function,
    which would highlight the next section to be typed
    
    Implemented code to move to the next section after
    current section has been typed correctly.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 //Quotes database
 const quotesDatabase = [
-    'They conquire, who believe the can.',
+    'They conquer, who believe they can.',
     'I know of no more encouraging fact than the ability of man to control himself through concious endeavour.',
-    'Whatever the min of man can conceive and belive, it can achieve.',
+    'Whatever the mind of man can conceive and believe, it can achieve.',
     'Give me a lever and I will move the earth.'
 ]
 
@@ -14,15 +14,11 @@ const quoteBoard = document.querySelector('#quote-board');
 const playerResponse = document.querySelector('#player-response');   
 
 
-//Other global variables
-let currentWordMatched = false;
-let nextToMatchIndex = 0;
-let matchedSection = '';
-
-
 //Global variables for the quote matiching section
-let  quoteSections;
-let sectionIndex;
+let quoteSections;
+let sectionIndex = 0;
+let matchedSections = '';
+let nextSection;
 
  
  //This sections contains code that generate and display a random quote.
@@ -40,6 +36,8 @@ function generateRandomQuotes(e) {
     let randomIndex = Math.floor(Math.random() * quotes.length);
     randomQuote = quotes[randomIndex];
     displayRandomQuote(randomQuote);
+    //Reset sectionIndex to zero
+    sectionIndex = 0
 }
 
 
@@ -84,45 +82,51 @@ playButton.addEventListener('click', generateRandomQuotes);
 //player types and also determine the time taken to complete the match.
 
 function startTyping(e) {
-    e.preventDefault();
+    // console.log(playerResponse.value = e.target.value);
+
+    const quote = document.querySelector('#quote-board #quote');
+    if (quote == null) {
+        return;  
+    }
+
+    //Append a space to each section coming from the quotesSection array except the last one
+    if (sectionIndex !== quoteSections.length - 1) {
+        nextSection = `${quoteSections[sectionIndex]} `;
+    } else {
+        nextSection = quoteSections[sectionIndex];
+    }
     
+    highlightNextSection(nextSection, '#quote-board #quote');
+    let sectionsToMatch = `${matchedSections}${nextSection}`;
+
+    if ((playerResponse.value == sectionsToMatch)) {
+        matchedSections = sectionsToMatch;
+        console.log(playerResponse.value, sectionsToMatch);
+        typeNextSection();
+    }
     
-    // let 
-    let wordToMatch = getNextToMatch();
-    console.log(wordToMatch);
-
-    let sectionToMatch = `${matchedSection}${wordToMatch}`
-
-    if (playerResponse.value === sectionToMatch) {
-        matchedSection += sectionToMatch;
-
-        //Indicate the next word to match
-        highlightNextWord(wordToMatch)
-        //
-
-        // currentWordMatched = true;
-        // wordTomatch = matchEntireQuote();       
-    }  
+    console.log(sectionIndex);
+    //Highlight next section to be matched
 }
 
 
-function highlightNextWord(wordToMatch) {
-    const quote = document.querySelector('#quote-board #quote').textContent;
-    const sections = quote.split(wordToMatch);
-    quote.innerHTML = `${sections[0]}<span style="color: blue;">${wordToMatch}</span>${sections[1]}`
+function highlightNextSection(nextSection, target) {
+    const originalQuote = document.querySelector(target).textContent;
+    const highlightedQuote = document.createElement('p');
+    highlightedQuote.id = 'quote';
+    const sections = originalQuote.split(`${nextSection}`);
+    highlightedQuote.innerHTML = `${sections[0]}<span id="next-section" style="color: blue; font-size: 24px;">${nextSection}</span>${sections[1]}`
+    removePreviousValue(target);
+    quoteBoard.append(highlightedQuote);
 }
 
 
-const getSection = () => {
-    let section;
-
+function typeNextSection() {
     while (sectionIndex < quoteSections.length) {
-        section = quoteSections[sectionIndex];
         sectionIndex += 1;
         break;
     }
-    return section;
 }
 
-playerResponse.addEventListener('focus', startTyping);
-//End of section
+playerResponse.addEventListener('keypress', startTyping);
+// //End of section
